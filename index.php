f0f3cf11d90a9eed8f9b033964a93003138fa855
Power-Cash <habibalejalil@gmail.com>
2023-02-23 21:14:09 +0300

    Nomics v1
    put you domain instead of example.com

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+// Define the API URL
+$url = 'https://www.example.com/api/v2/trade/coingecko/tickers';
+
+// Make the API request and retrieve the response
+$apiResponse = file_get_contents($url);
+
+// Parse the API response as an array
+$responseArray = json_decode($apiResponse, true);
+
+// Define an array to hold the converted data
+$convertedData = array();
+
+// Loop through each ticker in the response
+foreach ($responseArray as $ticker) {
+  // Extract the base and target currencies from the ticker_id
+  $tickerId = $ticker["ticker_id"];
+  $currencies = explode("_", $tickerId);
+  $baseCurrency = $currencies[0];
+  $quoteCurrency = $currencies[1];
+
+  // Construct the market name in the desired format
+  $market = $baseCurrency . "-" . $quoteCurrency;
+
+  // Extract the relevant data from the ticker and add it to the converted data array
+  $convertedData[] = array(
+    "market" => $market,
+    "base" => $baseCurrency,
+    "quote" => $quoteCurrency,
+    "price_quote" => $ticker["last_price"],
+    "volume_base" => $ticker["base_volume"]
+  );
+}
+
+// Encode the converted data as a JSON string
+$convertedDataJson = json_encode($convertedData);
+
+// Print the converted data
+echo $convertedDataJson;
+
+
+?>
