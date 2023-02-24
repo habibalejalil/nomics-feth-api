8e206372560ffa2c5792ec5b0940053085a3e01a
Habib Alejalil <habibalejalil@gmail.com>
2023-02-24 03:26:45 +0300

    Update index.php

--- markets/index.php
+++ markets/index.php
@@ -1,35 +1,22 @@
 <?php
+$url = 'https://www.adonis.exchange/api/v2/trade/public/markets';
+$apiResponse = file_get_contents($url);
+$markets = json_decode($apiResponse, true);
 
-// Set API endpoint URL
-$url = 'https://www.example.com/api/v2/trade/public/markets';
+$result = array();
+foreach ($markets as $market) {
+  $base = strtoupper($market['base_unit']);
+  $quote = strtoupper($market['quote_unit']);
+  $id = $base . '_' . $quote;
 
-// Initialize cURL session
-$curl = curl_init($url);
-
-// Set cURL options
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-// Execute cURL request and store response in $response variable
-$response = curl_exec($curl);
-
-// Close cURL session
-curl_close($curl);
-
-// Decode the JSON response into an associative array
-$data = json_decode($response, true);
-
-// Loop through each market in the data array and format it
-foreach ($data as $market) {
-    $formattedMarket = [
-        'id' => str_replace('/', '_', $market['name']),
-        'type' => 'spot',
-        'base' => $market['base_unit'],
-        'quote' => $market['quote_unit']
-    ];
-
-    // Encode the formatted market as a JSON object
-    $json = json_encode($formattedMarket);
-
-    // Output the JSON object on a new line with a line break character
-    echo $json . "\n";
+  $entry = array(
+    'id' => $id,
+    'type' => 'spot',
+    'base' => $base,
+    'quote' => $quote
+  );
+  array_push($result, $entry);
 }
+$json = json_encode($result, JSON_PRETTY_PRINT);
+echo nl2br($json);
+?>
