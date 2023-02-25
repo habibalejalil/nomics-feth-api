0da2abdfa3de8c74a048e599eddf43faf14cc6a0
Power-Cash <habibalejalil@gmail.com>
2023-02-25 20:54:26 +0300

    v2

---
+++ markets/index.php
@@ -0,0 +1,24 @@
+<?php
+$url = 'https://www.example.exchange/api/v2/trade/public/markets';
+$apiResponse = file_get_contents($url);
+$markets = json_decode($apiResponse, true);
+
+$result = array();
+foreach ($markets as $market) {
+  $base = strtoupper($market['base_unit']);
+  $quote = strtoupper($market['quote_unit']);
+  $id = $base . '_' . $quote;
+
+  $entry = array(
+    'id' => $id,
+    'type' => 'spot',
+    'base' => $base,
+    'quote' => $quote
+  );
+  array_push($result, $entry);
+}
+$json = json_encode($result, JSON_PRETTY_PRINT);
+
+header('Content-Type: application/json');
+echo $json;
+?>
